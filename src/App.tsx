489e996c2784a7ae1c5c3b907f10f50818e3d492
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-02-10 23:00:24 -0300

    Ajustando animação de compra de carta e fazendo a adiçao da carta comprada

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Card from './components/Card/Card';
 import Hand from './components/Hand/Hand';
-import { CardNaipes, CardNumbers } from './types/Card';
+import { CardNaipes, CardNumbers, ICard } from './types/Card';
 
 function App() {
 
@@ -10,6 +10,57 @@ function App() {
   const [drawCardVisibleSide, setDrawCardVisibleSide] = useState<"front" | "back">("back");
   const [drawCardNumber, setDrawCardNumber] = useState<CardNumbers>();
   const [drawCardNaipe, setDrawCardNaipe] = useState<CardNaipes>();
+
+  const cardsInicial: ICard[] = [
+    {
+      id: 1,
+      naipe: "hearts",
+      number: 1
+    },
+    {
+      id: 2,
+      naipe: "hearts",
+      number: 2
+    },
+    {
+      id: 3,
+      naipe: "hearts",
+      number: 3
+    },
+    {
+      id: 4,
+      naipe: "hearts",
+      number: 4
+    },
+    {
+      id: 5,
+      naipe: "hearts",
+      number: 5
+    },
+    {
+      id: 6,
+      naipe: "hearts",
+      number: 6
+    },
+    {
+      id: 7,
+      naipe: "hearts",
+      number: 7
+    },
+    {
+      id: 8,
+      naipe: "hearts",
+      number: 8
+    },
+    {
+      id: 9,
+      naipe: "hearts",
+      number: 9
+    },
+  ]
+
+  const [cards, setCards] = useState<ICard[]>(cardsInicial);
+
 
 
   const drawCard = () => {
@@ -24,15 +75,24 @@ function App() {
     }, 300);
 
     setTimeout(() => {
+      setCards((cards) =>
+        [...cards, { id: 515, naipe: "spades", number: 12 }]
+      );
+    }, 1400);
+
+    setTimeout(() => {
       setDrawCardVisibleSide("back");
       setDrawCardNumber(undefined);
       setDrawCardNaipe(undefined);
+
     }, 1900);
 
     setTimeout(() => {
       setDraw(false);
     }, 2700);
   }
+
+
 
   return (
     <>
@@ -44,7 +104,7 @@ function App() {
 
         <section className="main-side">
           <ul className="trash">
-            
+
             <li><Card number={3} naipe="spades" visibleSide={'front'} /></li>
             <li><Card number={3} naipe="diamonds" visibleSide={'front'} /></li>
             <li><Card number={3} naipe="hearts" visibleSide={'front'} /></li>
@@ -59,6 +119,7 @@ function App() {
             <li><Card visibleSide={'back'} /></li>
             <li><Card visibleSide={'back'} /></li>
             <li><Card visibleSide={'back'} /></li>
+
             <li onClick={drawCard}>
               <Card visibleSide={drawCardVisibleSide} number={drawCardNumber} naipe={drawCardNaipe} />
             </li>
@@ -67,7 +128,7 @@ function App() {
         </section>
 
         <section className="my-side">
-          <Hand cards={[]} isAdversaryHand={false} />
+          <Hand cards={cards} isAdversaryHand={false} />
         </section>
 
       </main>
--- src/components/Hand/Hand.tsx
+++ src/components/Hand/Hand.tsx
@@ -1,6 +1,7 @@
 import "./Hand.css";
 import { ICard } from '../../types/Card';
 import Card from '../Card/Card';
+import { useEffect } from "react";
 
 type HandProps = {
   cards: ICard[]
@@ -11,42 +12,49 @@ const Hand = ({ cards, isAdversaryHand }: HandProps) => {
 
   const visibleSide = isAdversaryHand ? "back" : "front";
 
+  useEffect(() => {
+    
+  },[cards]);
+
   return (
     <ul className={`hand ${isAdversaryHand ? "hand-adversary" : ''}`}>
-      <li >
-        <Card naipe={"diamonds"} number={3} visibleSide={visibleSide} />
+      {/* <li >
+        <Card naipe={"spades"} number={12} visibleSide={visibleSide} />
       </li>
       <li >
-        <Card naipe={"diamonds"} number={3} visibleSide={visibleSide} />
+        <Card naipe={"spades"} number={12} visibleSide={visibleSide} />
       </li>
       <li >
-        <Card naipe={"diamonds"} number={3} visibleSide={visibleSide} />
+        <Card naipe={"spades"} number={12} visibleSide={visibleSide} />
       </li>
       <li >
-        <Card naipe={"diamonds"} number={3} visibleSide={visibleSide} />
+        <Card naipe={"spades"} number={12} visibleSide={visibleSide} />
       </li>
       <li >
-        <Card naipe={"diamonds"} number={3} visibleSide={visibleSide} />
+        <Card naipe={"spades"} number={12} visibleSide={visibleSide} />
       </li>
       <li >
-        <Card naipe={"diamonds"} number={3} visibleSide={visibleSide} />
+        <Card naipe={"spades"} number={12} visibleSide={visibleSide} />
       </li>
       <li >
-        <Card naipe={"diamonds"} number={3} visibleSide={visibleSide} />
+        <Card naipe={"spades"} number={12} visibleSide={visibleSide} />
       </li>
       <li >
-        <Card naipe={"diamonds"} number={3} visibleSide={visibleSide} />
+        <Card naipe={"spades"} number={12} visibleSide={visibleSide} />
       </li>
       <li >
-        <Card naipe={"diamonds"} number={3} visibleSide={visibleSide} />
+        <Card naipe={"spades"} number={12} visibleSide={visibleSide} />
       </li>
+      <li >
+        <Card naipe={"spades"} number={12} visibleSide={visibleSide} />
+      </li> */}
 
 
-      {/* {cards.length > 0 && cards.map((card:ICard) => (
+      {cards.length > 0 && cards.map((card:ICard) => (
         <li key={card.id}>
-          <Card naipe={card.naipe} number={card.number} />
+          <Card naipe={card.naipe} number={card.number} visibleSide={visibleSide} />
         </li>
-      ))} */}
+      ))}
     </ul>
   )
 }
